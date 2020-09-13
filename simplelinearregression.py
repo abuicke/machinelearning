2aa6a35a0adc3cc5a8a0e73c02694da549b7778b
abuicke <mosesjayjayjay@gmail.com>
2020-09-13 08:21:15 +0100

    removed import aliases for simplelinearregression

--- simplelinearregression.py
+++ simplelinearregression.py
@@ -1,30 +1,30 @@
 import matplotlib.pyplot as plt
-import pandas as pd
-import pylab as pl
-import numpy as np
+import pandas
+import pylab
+import numpy
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
 # Load dataset
-df = pd.read_csv("emobank.csv")
+df = pandas.read_csv("emobank.csv")
 
 # Split data set into train and test
-msk = np.random.rand(len(df)) < 0.8
+msk = numpy.random.rand(len(df)) < 0.8
 train = df[msk]
 test = df[~msk]
 
 regr = LinearRegression()
-train_x = np.asanyarray(train[['V']])
-train_y = np.asanyarray(train[['D']])
+train_x = numpy.asanyarray(train[['V']])
+train_y = numpy.asanyarray(train[['D']])
 regr.fit (train_x, train_y)
 # The coefficients
 print ('Coefficients: ', regr.coef_)
 print ('Intercept: ',regr.intercept_)
 
-test_x = np.asanyarray(test[['V']])
-test_y = np.asanyarray(test[['D']])
+test_x = numpy.asanyarray(test[['V']])
+test_y = numpy.asanyarray(test[['D']])
 test_y_ = regr.predict(test_x)
 
-print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
-print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("Mean absolute error: %.2f" % numpy.mean(numpy.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % numpy.mean((test_y_ - test_y) ** 2))
 print("R2-score: %.2f" % r2_score(test_y_ , test_y))
