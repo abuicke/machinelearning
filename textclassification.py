dfdbf5ba1d1bef1da6a007235c04803e44db527c
abuicke <mosesjayjayjay@gmail.com>
2020-09-13 13:42:47 +0100

    text classification, downloaded dataset, imported into project, sanitized text

---
+++ textclassification.py
@@ -0,0 +1,46 @@
+import os
+import numpy
+import re
+import pickle
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+from sklearn.datasets import load_files
+from nltk.corpus import stopwords
+
+movie_data = load_files("data/txt_sentoken")
+X, y = movie_data.data, movie_data.target
+
+documents = []
+
+from nltk.stem import WordNetLemmatizer
+
+stemmer = WordNetLemmatizer()
+
+for sen in range(0, len(X)):
+    # Remove all the special characters
+    document = re.sub(r'\W', ' ', str(X[sen]))
+
+    # remove all single characters
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+
+    # Remove single characters from the start
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
+
+    # Substituting multiple spaces with single space
+    document = re.sub(r'\s+', ' ', document, flags=re.I)
+
+    # Removing prefixed 'b'
+    document = re.sub(r'^b\s+', '', document)
+
+    # Converting to Lowercase
+    document = document.lower()
+
+    # Lemmatization
+    document = document.split()
+
+    document = [stemmer.lemmatize(word) for word in document]
+    document = ' '.join(document)
+
+    documents.append(document)
+    
